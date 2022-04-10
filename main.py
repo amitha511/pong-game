e3b9b31f6f4e00416d4e40ac4d43272d3f9c9413
amitha511 <amitha51111@gmail.com>
2022-04-11 00:17:15 +0300

    paddle and ball collisions

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import turtle
 # windows
 wn = turtle.Screen()
 wn.title("pong by Amit Halfon")
-wn.bgcolor("pink")
+wn.bgcolor("#EBDEF0")
 wn.setup(width=800, height=600)
 wn.tracer(0)
-
 
 # paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("black")
+paddle_a.color("#1B2631")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)  # size
 paddle_a.penup()
 paddle_a.goto(-350, 0)
@@ -21,7 +20,7 @@ paddle_a.goto(-350, 0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("black")
+paddle_b.color("#1B2631")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)  # size
 paddle_b.penup()
 paddle_b.goto(350, 0)
@@ -30,7 +29,7 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
-ball.color("black")
+ball.color("#1B2631")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.20  # data change x
@@ -39,24 +38,28 @@ ball.dy = -0.20  # data change y
 
 # func
 def paddle_a_up():
+    if paddle_a.ycor() <= 240:  # for border
         y = paddle_a.ycor()
         y += 20  # up
         paddle_a.sety(y)
 
 
 def paddle_a_down():
-         y = paddle_a.ycor()
-         y -= 20  # up
-         paddle_a.sety(y)
+    if paddle_a.ycor() >= -230:
+        y = paddle_a.ycor()
+        y -= 20  # up
+        paddle_a.sety(y)
 
 
 def paddle_b_up():
+    if paddle_b.ycor() <= 240:
         y = paddle_b.ycor()
         y += 20  # up
         paddle_b.sety(y)
 
 
 def paddle_b_down():
+    if paddle_b.ycor() >= -230:
         y = paddle_b.ycor()
         y -= 20  # up
         paddle_b.sety(y)
@@ -70,7 +73,7 @@ wn.onkeypress(paddle_a_down, "S")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
-
+wn.onkeyrelease(wn.bye, "Escape")
 
 # main game loop
 while True:
@@ -91,12 +94,19 @@ while True:
 
     if ball.xcor() > 390:
         ball.setx(390)
+        ball.goto(0, 0)  # new round
         ball.dx *= -1
-
 
     if ball.xcor() < -390:
         ball.setx(-390)
+        ball.goto(0, 0)  # new round
         ball.dx *= -1
 
+    # paddle and ball collisions
+    if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
 
-
+    if (-340 > ball.xcor() > -350) and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
