ba7d0df6616925ec2c4c161477a74948b94c27a4
amitha511 <amitha51111@gmail.com>
2022-04-11 10:30:54 +0300

    Add players and score

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ wn.title("pong by Amit Halfon")
 wn.bgcolor("#EBDEF0")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+name_a = wn.textinput("player A", "Please enter the name of player A:")
+name_b = wn.textinput("player B", "Please enter the name of player B:")
+
+# Score
+score_a = 0
+score_b = 0
 
 # paddle A
 paddle_a = turtle.Turtle()
@@ -34,6 +40,22 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.20  # data change x
 ball.dy = -0.20  # data change y
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("#1B2631")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+
+
+def penUpDate():
+    pen.clear()
+    pen.write("{}: {}          {}: {}".format(name_a, score_a, name_b, score_b), align="center", font=("@Dotum", 24, "normal"))
+
+
+penUpDate()
 
 
 # func
@@ -96,11 +118,15 @@ while True:
         ball.setx(390)
         ball.goto(0, 0)  # new round
         ball.dx *= -1
+        score_a += 1
+        penUpDate()
 
     if ball.xcor() < -390:
         ball.setx(-390)
         ball.goto(0, 0)  # new round
         ball.dx *= -1
+        score_b += 1
+        penUpDate()
 
     # paddle and ball collisions
     if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 50):
